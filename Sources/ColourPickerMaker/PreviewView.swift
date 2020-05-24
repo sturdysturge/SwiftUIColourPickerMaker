f94b2a7963720856f7e4357ca8a7c09dfa19d90b
robsturgeon <github@sturdysturge.com>
2020-05-24 11:07:12 +0100

    Added images folder for README

--- Sources/ColourPickerMaker/PreviewView.swift
+++ Sources/ColourPickerMaker/PreviewView.swift
@@ -7,7 +7,7 @@ public struct PreviewView: View {
   public var body: some View {
     VStack {
       PreviewColorView(colour: colourModel.colour, square: true)
-      ColourWheelView(rotation: $colourModel.hue, distanceFromCentre: $colourModel.saturation, angularGradient: GradientType.hue.gradient, radialGradient: GradientType.saturationOverlay.gradient)
+      GridPaletteView(canvasType: .hueSaturation, verticalSwatches: 10, horizontalSwatches: 10, hue: $colourModel.hue, saturation: $colourModel.saturation, brightness: $colourModel.brightness, alpha: $colourModel.alpha)
     }
   }
 }
