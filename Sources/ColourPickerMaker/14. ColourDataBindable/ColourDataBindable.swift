7a34d241947cb63e7c785ee44a9e080448d4a232
robsturgeon <github@sturdysturge.com>
2020-06-09 20:16:11 +0100

    Added public initialiser for ColourDataBindable

--- Sources/ColourPickerMaker/14. ColourDataBindable/ColourDataBindable.swift
+++ Sources/ColourPickerMaker/14. ColourDataBindable/ColourDataBindable.swift
@@ -10,38 +10,58 @@
 import SwiftUI
 
 protocol ColourDataBindable {
-    associatedtype ValueType
-    var values: ValueType { get }
-    var _$values: Binding<ValueType> { get }
-    func bindingValues() -> (x: Binding<Double>, y: Binding<Double>)
-    var parameters: Parameter.Pair { get }
-    func getBackground() -> Color
+  associatedtype ValueType
+  var values: ValueType { get }
+  var _$values: Binding<ValueType> { get }
+  func bindingValues() -> (x: Binding<Double>, y: Binding<Double>)
+  var parameters: Parameter.Pair { get }
+  func getBackground() -> Color
 }
 
 public struct RGBAData: ColourDataBindable {
-    public typealias ValueType = ColourModel.RGBAValues
-    @Binding var values: ValueType
-    var _$values: Binding<ValueType> { _values }
-    let parameters: Parameter.Pair
+  public typealias ValueType = ColourModel.RGBAValues
+  @Binding var values: ValueType
+  var _$values: Binding<ValueType> { _values }
+  let parameters: Parameter.Pair
+  
+  public init(values: Binding<ValueType>, parameters: Parameter.Pair) {
+    self._values = values
+    self.parameters = parameters
+  }
 }
 
 public struct HSBAData: ColourDataBindable {
-    public typealias ValueType = ColourModel.HSBAValues
-    @Binding var values: ValueType
-    var _$values: Binding<ValueType> { _values }
-    let parameters: Parameter.Pair
+  public typealias ValueType = ColourModel.HSBAValues
+  @Binding var values: ValueType
+  var _$values: Binding<ValueType> { _values }
+  let parameters: Parameter.Pair
+  
+  public init(values: Binding<ValueType>, parameters: Parameter.Pair) {
+    self._values = values
+    self.parameters = parameters
+  }
 }
 
 public struct CMYKAData: ColourDataBindable {
-    public typealias ValueType = ColourModel.CMYKAValues
-    @Binding var values: ValueType
-    var _$values: Binding<ValueType> { _values }
-    let parameters: Parameter.Pair
+  public typealias ValueType = ColourModel.CMYKAValues
+  @Binding var values: ValueType
+  var _$values: Binding<ValueType> { _values }
+  let parameters: Parameter.Pair
+  
+  public init(values: Binding<ValueType>, parameters: Parameter.Pair) {
+    self._values = values
+    self.parameters = parameters
+  }
 }
 
 public struct GreyscaleData: ColourDataBindable {
-    public typealias ValueType = ColourModel.GreyscaleValues
-    @Binding var values: ValueType
-    var _$values: Binding<ValueType> { _values }
-    let parameters: Parameter.Pair
+  public typealias ValueType = ColourModel.GreyscaleValues
+  @Binding var values: ValueType
+  var _$values: Binding<ValueType> { _values }
+  let parameters: Parameter.Pair
+  
+  public init(values: Binding<ValueType>, parameters: Parameter.Pair) {
+    self._values = values
+    self.parameters = parameters
+  }
 }
