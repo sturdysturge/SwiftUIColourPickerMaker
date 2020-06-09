b135c55691cddf552479ca89562a46b379d4374a
robsturgeon <github@sturdysturge.com>
2020-06-09 18:59:18 +0100

    Removed Xcode projects

--- ColourPickerMaker/README.md
+++
@@ -1,3 +0,0 @@
-# ColourPickerMaker
-
-A description of this package.
--- Package.swift
+++ Package.swift
@@ -1,13 +1,15 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "ColourPickerMaker",
-    platforms: [.iOS(.v13), .macOS(.v10_15)],
-    products: [
-        .library(
-            name: "ColourPickerMaker",
-            targets: ["ColourPickerMaker"]),
-    ],
+    platforms: [.iOS(.v13), .macOS(.v10_15)], products: [
+      .library(
+        name: "ColourPickerMaker",
+        targets: ["ColourPickerMaker"]),
+  ],
     dependencies: [
     ],
     targets: [
